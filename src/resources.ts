c0e604a01cd1828dfac3ae74e3b01a76fb243582
Cellsito <celsobfigueroa@gmail.com>
2024-06-05 17:23:30 -0300

    Feat: gamification scene

--- src/resources.ts
+++ src/resources.ts
@@ -1,10 +1,12 @@
 import { ImageSource, Loader } from "excalibur";
 import sword from "./images/sword.png";
 import logo from "./images/logo.png";
+import logo_vert from "./images/logo-vertical.png"
 
 export const Resources = {
   Sword: new ImageSource(sword),
   Logo: new ImageSource(logo),
+  Logo_v: new ImageSource(logo_vert)
 } as const;
 
 export const loader = new Loader();
--- src/scenes/historyScene.ts
+++ src/scenes/historyScene.ts
@@ -1,6 +1,9 @@
-import { Color, Engine, FadeInOut, Scene, Transition } from "excalibur";
+import { Actor, Color, Engine, FadeInOut, Keys, Scene, Transition, vec } from "excalibur";
+import { Resources } from "../resources";
 
 export class historyScene extends Scene {
+    // declaração do elemento texto
+    elementoTexto?: HTMLElement
 
     onTransition(direction: "in" | "out"): Transition | undefined {
         return new FadeInOut({
@@ -11,6 +14,52 @@ export class historyScene extends Scene {
     }
 
     onInitialize(engine: Engine<any>): void {
-        this.backgroundColor = Color.Violet
+        this.backgroundColor = Color.fromHex("#403f4c")
+
+        // criar elemento com descrição de empresa
+        this.elementoTexto = document.createElement("div") as HTMLElement
+        // opacidade = 1 visível
+        this.elementoTexto.style.opacity = "1"
+
+        // inserir elementoTexto no container-game
+        let containerGame = document.querySelector(".container-game") as HTMLElement
+        containerGame.appendChild(this.elementoTexto)
+
+        // adicionar classe na div criada (elementoTexto)
+        this.elementoTexto.classList.add("sobre-gamifica")
+
+        // adiciona titulo de paragráfo dentro da div
+        this.elementoTexto.innerHTML = `<h2>Sobre o GamificaAI</h2>
+        <p>Nossa empresa cria soluções de gamificação personalizadas para empresas de todos os tamanhos e setores,
+          usando inteligência artificial e design de jogos para desenvolver estratégias interativas que melhoram a
+          experiência do usuário e impulsionam resultados. Acreditamos no poder dos jogos e da tecnologia para engajar
+          equipes, aumentar a produtividade e motivar, adaptando cada projeto às necessidades específicas do cliente,
+          desde programas de treinamento interativo até sistemas de recompensa e engajamento de funcionários.
+        </p>`
+
+        // actor logo vertical
+        let actorLogoV = new Actor ({
+            pos: vec(engine.drawWidth - 300, engine.halfDrawHeight - 50)
+        })
+
+        // carregando imagem do logoV
+        let imagemLogoV = Resources.Logo_v.toSprite()
+
+        // config tamanho/zoom
+        imagemLogoV.scale = vec (0.7, 0.7)
+
+        // add imagem no actor
+        actorLogoV.graphics.add(imagemLogoV)
+        
+        // renderiza actor na cena
+        this.add(actorLogoV)
+
+        // monitora tecla pressionada
+        this.input.keyboard.on("press", (event) => {
+            // caso pressionado "Enter", próxima cena
+            if (event.key == Keys.Enter) {
+                engine.goToScene("gamificacao")
+            }
+        })
     }
 }
