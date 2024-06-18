4603b2af91126ff608ccf4f8a94d63f0464587f3
Cellsito <celsobfigueroa@gmail.com>
2024-06-18 17:23:43 -0300

    Feat: add de imagens

--- src/resources.ts
+++ src/resources.ts
@@ -1,8 +1,13 @@
-import { ImageFiltering, ImageSource, Loader } from "excalibur";
+import { ImageFiltering, ImageSource, Loader, Sound } from "excalibur";
 import sword from "./images/sword.png";
 import logo from "./images/logo.png";
 import logo_vert from "./images/logo-vertical.png";
 import gamificas from "./images/gmificacion.png";
+
+import phoenix from "./images/phoenix.png";
+import delsin from "./images/delsin.png";
+import spy from "./images/spy.png"
+
 import { TiledResource } from "@excaliburjs/plugin-tiled";
 
 import pngTilesetPath from "./maps/Room_Builder_32x32.png?url"
@@ -16,12 +21,21 @@ import tmxMapaPath from "./maps/showroom.tmx?url"
 
 import playerSpritePath from "./sprites/Dante.png"
 
+import sariasong from "./sounds/zelda.mp3"
+
 export const Resources = {
   Sword: new ImageSource(sword),
   Logo: new ImageSource(logo),
   Logo_v: new ImageSource(logo_vert),
   PlayerSpriteSheet: new ImageSource(playerSpritePath, { filtering: ImageFiltering.Pixel }),
   Gamificasa: new ImageSource(gamificas),
+
+  Phoenix: new ImageSource(phoenix),
+  Delsin: new ImageSource(delsin),
+  Spy: new ImageSource(spy),
+
+  Saria_s: new Sound(sariasong),
+
   Mapa: new TiledResource(tmxMapaPath, {
     pathMap: [
       {path: "showroom.tmx", output: tmxMapaPath},
